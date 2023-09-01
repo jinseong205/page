1e8bc42d5705f491a6b8532d4e8015510dda4b34
daengdaengLee <gunho1020@gmail.com>
2023-09-01 20:24:12 +0900

    fix: PageResponse 와 CurrentPageResponse 분리
    - 재귀적으로 계속해서 subPage 로 파고들 수 있다고 판단

--- src/main/java/com/hello/page/web/response/PageResponse.java
+++ src/main/java/com/hello/page/web/response/PageResponse.java
@@ -3,14 +3,10 @@ package com.hello.page.web.response;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class PageResponse {
     private String pageId;
     private String title;
     private String content;
-    private List<PageResponse> subPages;
-    private List<BreadcrumbResponse> breadcrumbs;
 }
