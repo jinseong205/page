7ffbb54e3ffc8522d4da0807daad1ec80ca59d2b
daengdaengLee <gunho1020@gmail.com>
2023-09-01 21:09:00 +0900

    feat: 부모 페이지 아이디로 자식 페이지 목록 조회하는 Repository 메소드 선언
    - 인터페이스만 선언
    - PageRepository#findByParentId

--- src/main/java/com/hello/page/application/page/outboundport/PageRepository.java
+++ src/main/java/com/hello/page/application/page/outboundport/PageRepository.java
@@ -2,8 +2,11 @@ package com.hello.page.application.page.outboundport;
 
 import com.hello.page.application.page.domain.Page;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PageRepository {
     Optional<Page> findById(Long id);
+
+    List<Page> findByParentId(Long parentId);
 }
