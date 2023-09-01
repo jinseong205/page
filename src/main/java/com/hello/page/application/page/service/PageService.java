477c677a4c44b2c79afff321e2bf0d07ee0ee5e1
daengdaengLee <gunho1020@gmail.com>
2023-09-01 21:33:26 +0900

    feat: PageService#readPage 에서 Breadcrumb 구성해서 응답에 추가
    - Breadcrumb 쉽게 구성하기 위해 @Builder 추가
    - 부모 페이지 아이디 목록 -> WHERE IN ids -> 부모 페이지 목록 흐름
    - 부모 페이지 아이디는 있는데 부모 페이지 조회에서 빠진 경우 예외로 처리

--- src/main/java/com/hello/page/application/page/service/PageService.java
+++ src/main/java/com/hello/page/application/page/service/PageService.java
@@ -1,11 +1,16 @@
 package com.hello.page.application.page.service;
 
+import com.hello.page.application.page.domain.Breadcrumb;
+import com.hello.page.application.page.domain.Page;
 import com.hello.page.application.page.domain.ParentPage;
 import com.hello.page.application.page.outboundport.PageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Service
@@ -17,9 +22,31 @@ public class PageService {
                 .findById(pageId)
                 .map(page -> {
                     var subPages = this.pageRepository.findByParentId(page.getId());
+
+                    var parentPageIds = this.pageRepository.findParentPageIds(page.getId());
+                    var parentPageMap = this.pageRepository
+                            .findAllById(parentPageIds)
+                            .stream()
+                            .collect(Collectors.toMap(Page::getId, Function.identity()));
+                    var breadcrumbs = IntStream.range(0, parentPageIds.size())
+                            .map(i -> parentPageIds.size() - 1 - i)
+                            .mapToObj(parentPageIds::get)
+                            .map(parentPageMap::get)
+                            .peek(parentPage -> {
+                                if (parentPage == null) {
+                                    throw new IllegalStateException("부모 페이지 중 찾을 수 없는 것이 있습니다.");
+                                }
+                            })
+                            .map(parentPage -> Breadcrumb.builder()
+                                    .pageId(parentPage.getId())
+                                    .title(parentPage.getTitle())
+                                    .build())
+                            .toList();
+
                     return ParentPage.builder()
                             .page(page)
                             .subPages(subPages)
+                            .breadcrumbs(breadcrumbs)
                             .build();
                 });
     }
