cca78d2ac2b35eb9a9ca4c9509dd22afe3d1af06
jinseong <jinseong205@gmail.com>
2023-09-01 18:08:55 +0900

    Initial commit

---
+++ src/test/java/com/hello/page/PageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
