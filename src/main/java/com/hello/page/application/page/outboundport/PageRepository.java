68abdccfaffde2e710e37616228462cc0834b086
daengdaengLee <gunho1020@gmail.com>
2023-09-01 21:30:34 +0900

    feat: PageRepository 에 부모 페이지 아이디 목록 조회 기능 추가
    - 현재 조회한 페이지 기준으로 브레드 크럼을 구성하기 위해 필요한 정보

--- src/main/java/com/hello/page/application/page/outboundport/PageRepository.java
+++ src/main/java/com/hello/page/application/page/outboundport/PageRepository.java
@@ -9,4 +9,9 @@ public interface PageRepository {
     Optional<Page> findById(Long id);
 
     List<Page> findByParentId(Long parentId);
+
+    /**
+     * 가장 가까운 부모부터 가장 먼 부모 순서대로 정렬
+     */
+    List<Long> findParentPageIds(Long id);
 }
