e4d6bcdee88b56e794fadb3f1d2673009ca5e111
daengdaengLee <gunho1020@gmail.com>
2023-09-02 23:38:44 +0900

    test: PageServiceImpl#readPage 테스트 작성
    - 페이지 조회 성공하는 경우
    - 동등 비교를 위한 @EqualsAndHashCode 추가

--- src/main/java/com/hello/page/domain/Page.java
+++ src/main/java/com/hello/page/domain/Page.java
@@ -2,12 +2,14 @@ package com.hello.page.domain;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 public class Page {
     @JsonUnwrapped
     private final PageInfo pageInfo;
--- src/test/java/com/hello/page/service/PageServiceImplTest.java
+++ src/test/java/com/hello/page/service/PageServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.hello.page.service;
 
+import com.hello.page.domain.Breadcrumb;
+import com.hello.page.domain.Page;
+import com.hello.page.domain.PageInfo;
 import com.hello.page.repository.PageRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,8 +31,70 @@ class PageServiceImplTest {
     @Test
     @DisplayName("존재하는 페이지를 조회하면 해당 페이지 정보를 담은 Optional<Page> 를 반환한다.")
     void readExistingPage() {
-        // @TODO readPage 완성 이후 테스트 작성
-        // mock 데이터는 controller 테스트에서 사용했던 데이터 재활용
+        // given
+        var grandParentPageInfo = PageInfo.builder()
+                .id(1L)
+                .title("부모-부모 페이지")
+                .content("이것은 부모-부모 페이지입니다.")
+                .parentId(null)
+                .build();
+        var parentPageInfo = PageInfo.builder()
+                .id(2L)
+                .title("부모 페이지")
+                .content("이것은 부모 페이지입니다.")
+                .parentId(grandParentPageInfo.getId())
+                .build();
+        var pageInfo = PageInfo.builder()
+                .id(3L)
+                .title("테스트 페이지")
+                .content("이것은 테스트 페이지입니다.")
+                .parentId(parentPageInfo.getId())
+                .build();
+        var childPageInfo1 = PageInfo.builder()
+                .id(4L)
+                .title("자식 페이지 1")
+                .content("이것은 첫 번째 자식 페이지입니다.")
+                .parentId(pageInfo.getId())
+                .build();
+        var childPageInfo2 = PageInfo.builder()
+                .id(5L)
+                .title("자식 페이지 2")
+                .content("이것은 두 번째 자식 페이지입니다.")
+                .parentId(pageInfo.getId())
+                .build();
+        var ancestorPages = List.of(parentPageInfo, grandParentPageInfo);
+        var subPages = List.of(childPageInfo1, childPageInfo2);
+        var grandParentBreadcrumb = Breadcrumb.builder()
+                .pageId(grandParentPageInfo.getId())
+                .title(grandParentPageInfo.getTitle())
+                .build();
+        var parentBreadcrumb = Breadcrumb.builder()
+                .pageId(parentPageInfo.getId())
+                .title(parentPageInfo.getTitle())
+                .build();
+        var breadcrumbs = List.of(grandParentBreadcrumb, parentBreadcrumb);
+        var page = Page.builder()
+                .pageInfo(pageInfo)
+                .subPages(subPages)
+                .breadcrumbs(breadcrumbs)
+                .build();
+        var ancestorPageIds = ancestorPages.stream().map(PageInfo::getId).toList();
+        when(this.pageRepository.findById(pageInfo.getId()))
+                .thenReturn(Optional.of(pageInfo));
+        when(this.pageRepository.findByParentId(pageInfo.getId()))
+                .thenReturn(subPages);
+        when(this.pageRepository.findParentPageIds(pageInfo.getId()))
+                .thenReturn(ancestorPageIds);
+        when(this.pageRepository.findAllById(ancestorPageIds))
+                .thenReturn(ancestorPages);
+
+        // when
+        var result = this.pageService.readPage(pageInfo.getId());
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+        var readPage = result.get();
+        assertThat(readPage).isEqualTo(page);
     }
 
     @Test
